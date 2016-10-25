90b070344351c16f2de649f7d44b5f4fa6c60f3a
lindezhi <linsony0@163.com>
2016-10-25 19:48:05 +0800

    misc:add read and write chan test

--- src/main/chantime.go
+++ src/main/chantime.go
@@ -6,6 +6,10 @@ import (
 	"time"
 )
 
+/**
+ * chan超时使用另外一个chan读取数据，使用go routine启动另一个sleep后释放
+ *
+ */
 func timeout(sec int) (ret chan bool, err error) {
 	if sec < 1 {
 		errors.New("invalid time")
@@ -36,6 +40,45 @@ func wait(ch chan int, tt int) (ret int, err error) {
 	}
 }
 
+type MyChan struct {
+	ich <-chan int
+	och chan<- int
+}
+
+/*
+ *
+ * read chan 需要用括号
+ */
+func newIntChan() MyChan {
+	var ch chan int = make(chan int)
+
+	var ich (<-chan int) = (<-chan int)(ch)
+	var och chan<- int = chan<- int(ch)
+	return MyChan{ich, och}
+}
+
+func iochanTest() {
+	var ch MyChan = newIntChan()
+	go func(out chan<- int) {
+		time.Sleep(time.Duration(3) * time.Second)
+		fmt.Println("write chan 10")
+		out <- 10
+		fmt.Println("write chan 20")
+		out <- 20
+	}(ch.och)
+
+	go func(in <-chan int) {
+		ret1 := <-in
+		fmt.Println("read chan :", ret1)
+		ret2 := <-in
+		fmt.Println("read chan :", ret2)
+	}(ch.ich)
+
+	fmt.Println("chan in out started")
+	time.Sleep(time.Duration(10) * time.Second)
+
+}
+
 func main() {
 	ch := make(chan int)
 	go func() {
@@ -48,5 +91,9 @@ func main() {
 	} else {
 		fmt.Println("chan wait ok ", ret)
 	}
+
+	//-----------------------------
+	fmt.Println("io chan start --------------")
+	iochanTest()
 
 }
