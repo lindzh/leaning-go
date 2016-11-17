a36486a58bc408555e7ad43255c3babbfabb207d
lindezhi <linsony0@163.com>
2016-11-17 19:39:16 +0800

    misc:add flag test

--- src/main/randomtest.go
+++ src/main/randomtest.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 )
 
 const RAND_SEED = 2
 
+/*
+*
+*flag 是命令行配置读取工具
+ */
 func main() {
 	source := rand.NewSource(RAND_SEED)
 	rd := rand.New(source)
 	fmt.Println(rd.Int31())
+
+	fmt.Println("flags test")
+
+	myint := flag.Int("myint", 123, "this is a int test")
+
+	fmt.Println("flag myint ", *myint)
 
 }
